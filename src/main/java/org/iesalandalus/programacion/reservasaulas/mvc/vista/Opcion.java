5da3c116afcbe631b362a84f9fb215303412ca43
danielcalatravagonzalez <degixcala@gmail.com>
2022-03-06 18:31:58 +0100

    Ejercicio 10: He creado la clase Consola con las respectivas modificaciones.

---
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/vista/Opcion.java
@@ -0,0 +1,129 @@
+package org.iesalandalus.programacion.reservasaulas.mvc.vista;
+
+public enum Opcion {
+	//Opciones del enum Opcion
+	SALIR("Salir") {
+		public void ejecutar() {
+			vista.salir();
+		}
+	},
+	INSERTAR_AULA("Insertar aula") {
+		public void ejecutar() {
+			vista.insertarAula();
+		}
+	},
+	BORRAR_AULA("Borrar aula") {
+		public void ejecutar() {
+			vista.borrarAula();
+		}
+	},
+	BUSCAR_AULA("Buscar aula") {
+		public void ejecutar() {
+			vista.buscarAula();
+		}
+	},
+	LISTAR_AULAS("Listar aulas") {
+		public void ejecutar() {
+			vista.listarAula();
+		}
+	},
+	INSERTAR_PROFESOR("Insertar profesor") {
+		public void ejecutar() {
+			vista.insertarProfesor();
+		}
+	},
+	BORRAR_PROFESOR("Borrar profesor") {
+		public void ejecutar() {
+			vista.borrarProfesor();
+		}
+	},
+	BUSCAR_PROFESOR("Buscar profesor") {
+		public void ejecutar() {
+			vista.buscarProfesor();
+		}
+	},
+	LISTAR_PROFESORES("Listar profesores") {
+		public void ejecutar() {
+			vista.listarProfesor();
+		}
+	},
+	INSERTAR_RESERVA("Insertar reserva") {
+		public void ejecutar() {
+			vista.realizarReserva();
+		}
+	},
+	BORRAR_RESERVA("Borrar reserva") {
+		public void ejecutar() {
+			vista.anularReserva();
+		}
+	},
+	LISTAR_RESERVAS("Listar reservas") {
+		public void ejecutar() {
+			vista.listarReservas();
+		}
+	},
+	LISTAR_RESERVAS_AULA("Listar reservas de aula") {
+		public void ejecutar() {
+			vista.listarReservasAula();
+		}
+	},
+	LISTAR_RESERVAS_PROFESOR("Listar reservas de profesor") {
+		public void ejecutar() {
+			vista.listarReservasProfesor();
+		}
+	},
+	LISTAR_RESERVAS_PERMANENCIA("Listar reservas de permanencia") {
+		public void ejecutar() {
+			vista.listarReservasPermanencia();
+		}
+	},
+	CONSULTAR_DISPONIBILIDAD("Consultar disponibilidad") {
+		public void ejecutar() {
+			vista.consultarDisponibilidad();
+		}
+	};
+	
+	//Atributos
+	private String mensajeAMostrar;
+	private static Vista vista;
+
+	//Método Opcion (String)
+	private Opcion(String mensaje) {
+		this.mensajeAMostrar = mensaje;
+	}
+
+	//Método getMensaje
+	public String getMensaje() {
+		return mensajeAMostrar;
+	}
+
+	//Método ejecutar
+	public abstract void ejecutar();
+
+	//Método setVista(Vista)
+	public static void setVista(Vista vista) {
+		if (vista == null) {
+			throw new NullPointerException("ERROR: La vista no pueda ser nula.");
+		}
+		Opcion.vista = vista;
+	}
+
+	//Método toString
+	@Override
+	public String toString() {
+		return String.format("%d.- %s", ordinal(), getMensaje());
+	}
+
+	//Método getOpcionSegunOrdinal
+	public static Opcion getOpcionSegunOrdinal(int ordinal) {
+		if (!esOrdinalValido(ordinal)) {
+			throw new IllegalArgumentException("Ordinal de la opción no válido");
+		}
+		return values()[ordinal];
+	}
+
+	//Método es OrdinalValido
+	public static boolean esOrdinalValido(int ordinal) {
+		return (ordinal >= 0 && ordinal <= values().length - 1);
+	}
+}
